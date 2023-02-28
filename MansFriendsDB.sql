9f6b78f7fc2222cbd004ae6a0ccf1cd9a1ca5234
PaulMart85 <outimer@mail.ru>
2023-02-28 16:22:13 +0600

    tasks 7-12 added

--- MansFriendsDB.sql
+++ MansFriendsDB.sql
@@ -3,214 +3,207 @@ CREATE DATABASE MansFriends;
 
 USE MansFriends;
 
-## Создаем низкоуровневые таблицы
-create table cat (
+-- таблицы представителей животных
+CREATE TABLE cat (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(100),
+    Command VARCHAR(50),
     Birthday DATE
 );
 
-
-create table dog (
+CREATE TABLE dog (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
     Birthday DATE
 );
 
-
-create table hamster (
+CREATE TABLE hamster (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
     Birthday DATE
 );
 
-
-
-create table horse (
+CREATE TABLE horse (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	liftWeight INT,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
 	Birthday DATE
 );
 
-
-create table camel (
+CREATE TABLE camel (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	liftWeight INT,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
 	Birthday DATE
 );
 
-
-create table donkey (
+CREATE TABLE donkey (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	liftWeight INT,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
 	Birthday DATE
 );
 
-## Создаем таблицы уровня выше
-create table pet (
+-- Таблицы групп животных
+CREATE TABLE homeAnimal (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
     Birthday DATE
 );
 
-
-create table packAnimal (
+CREATE TABLE packAnimal (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     liftWeight INT,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
     Birthday DATE
 );
 
-## Создаем общую таблицу
-create table mansFriend(
+-- базовый родительский класс
+CREATE TABLE mansFriend(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(45),
-    Command VARCHAR(45),
+    Name VARCHAR(50),
+    Command VARCHAR(100),
     Birthday Date
     );
     
-## Заполняем данными низкоуровневые таблицы
+-- Заполняем низкоуровневые таблицы представителей
+    INSERT INTO cat (Name, Command, Birthday) VALUES
+    ('Пуша', 'Спать', '2021-07-05'),
+    ('Глотик', 'Пить', '2019-04-06'),
+	('Беляш', 'Прыгать', '2021-01-02'),
+    ('Матроскин', 'Спать', '2015-03-10');
     
-    insert into cat (Name, Command, Birthday) values
-    ('Барсик', 'Спать', '2022-01-01'),
-    ('Мурзик', 'Спать', '2017-06-06'),
-	('Месси', 'Спать', '2023-01-06'),
-    ('Мурка', 'Спать', '2021-03-17');
-    
-    select * from cat;
+    SELECT * FROM cat;
 
-    insert into dog (Name, Command, Birthday) values
-    ('Полкан', 'Лежать', '2022-01-01'),
-    ('Хатико', 'Лежать', '2017-06-06'),
-	('Юта', 'Лежать', '2023-01-06'),
-    ('Микки', 'Лежать', '2021-03-17');
+    INSERT INTO dog (Name, Command, Birthday) VALUES
+    ('Бобик', 'Бежать', '2020-01-01'),
+    ('Барбос', 'Лежать', '2019-06-08'),
+	('Туман', 'Ползти', '2022-01-08'),
+    ('Дик', 'Лизать', '2023-04-17');
 	
-    select * from dog;
+    SELECT * FROM dog;
  
-	insert into hamster (Name, Command, Birthday) values
-    ('Хома', 'Бежать', '2022-01-01'),
-    ('Пушок', 'Бежать', '2017-06-06'),
-	('Хомура', 'Бежать', '2023-01-06'),
-    ('Генерал', 'Бежать', '2021-03-17');
+	INSERT INTO hamster (Name, Command, Birthday) VALUES
+    ('Томас', 'Кувырок', '2011-01-11'),
+    ('Фобка', 'Есть', '2019-01-15'),
+	('Крыска', 'Копать', '2020-08-16'),
+    ('Шарик', 'Бегать', '2016-04-27');
 	
-    select * from hamster;
+    SELECT * FROM hamster;
      
-	insert into camel (camelName, camelCommand, camelLiftWeight, camelBirthday) values
-    ('Агата', 'Поднять', 100, '2022-01-01'),
-    ('Ида', 'Поднять', 200, '2017-06-06'),
-	('Ланцелот', 'Поднять', 400, '2023-01-06'),
-    ('Джаред', 'Поднять', 50, '2021-03-17');
+	INSERT INTO camel (Name, Command, LiftWeight, Birthday) VALUES
+    ('Лихач', 'Встать', 90, '2012-10-11'),
+    ('Балда', 'Жевать', 20, '2015-08-11'),
+	('Водовоз', 'Поднимать', 40, '2021-02-16'),
+    ('Толик', 'Везти', 25, '2008-03-22');
 	
-    select * from camel;
+    SELECT * FROM camel;
      
-	insert into horse (Name, Command, liftWeight, Birthday) values
-    ('Мерин', 'Галоп', 150, '2022-01-01'),
-    ('Буран', 'Галоп', 50, '2017-06-06'),
-	('Плотва', 'Галоп', 200, '2023-01-06'),
-    ('Осман', 'Галоп', 250, '2021-03-17');
+	INSERT INTO horse (Name, Command, liftWeight, Birthday) VALUES
+    ('Хрусталик', 'Прыжок', 100, '2012-01-01'),
+    ('Топтышка', 'Рысь', 200, '2017-08-06'),
+	('Гончий', 'Галоп', 90, '2021-01-06'),
+    ('Кавказ', 'Рысь', 90, '2021-03-15');
 	
-    select * from horse;    
+    SELECT * FROM horse;    
      
-	insert into donkey (Name, Command, liftWeight, Birthday) values
-    ('Алдар', 'Жевать', 35, '2022-01-01'),
-    ('Бригелла', 'Жевать', 10, '2017-06-06'),
-	('Зигфрид', 'Жевать', 20, '2023-01-06'),
-    ('Моисей', 'Жевать', 100, '2021-03-17');
+	INSERT INTO donkey (Name, Command, liftWeight, Birthday) VALUES
+    ('Богарт', 'Бежать', 55, '2021-11-01'),
+    ('Пузик', 'Есть', 15, '2018-06-06'),
+	('Шрек', 'Кочевать', 200, '2021-08-12'),
+    ('Иа', 'Жевать', 10, '2018-09-11');
 	
-    select * from donkey;  	
+    SELECT * FROM donkey;  	
  
- ## Удаление таблицы верблюдов
- delete from camel where id > 0;
- select * from camel;
+ -- Удаление таблицы верблюдов
+ DELETE FROM camel WHERE id > 0;
+ SELECT * FROM camel;
  
-## Объединение таблицы лошадей и ослов в одну таблицу.
-create table packAnimalNew (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)
-select  Name, 
-        Command, 
-        Birthday
-from horse union 
-select  Name, 
-        Command, 
-        Birthday
-from donkey;
+-- Объединение таблицы лошадей и ослов в одну
+CREATE TABLE packAnimalNew (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)
+SELECT Name, 
+       Command, 
+       Birthday
+FROM horse UNION 
+SELECT Name, 
+       Command, 
+       Birthday
+FROM donkey;
 
-select * from packAnimalNew;
+SELECT * FROM packAnimalNew;
 
-## Создание таблицы, в которой все животные в возрасте от 1 до 3 лет.
-INSERT into pet (Name, Command, Birthday)
-select  Name, 
+-- cоздание таблицы c животныvb в возрасте от 1 до 3 лет
+INSERT INTO homeAnimal (Name, Command, Birthday)
+SELECT  Name, 
         Command, 
         Birthday
-from cat union 
-select  Name, 
+FROM cat UNION 
+SELECT  Name, 
         Command, 
         Birthday
-from dog union
-select  Name, 
+FROM dog UNION
+SELECT  Name, 
         Command, 
         Birthday
-from hamster;
-select * from pet;
+FROM hamster;
 
-INSERT into mansFriend (Name, Command, Birthday)
-select  Name, 
-        Command, 
-        Birthday
-from pet union 
-select  Name, 
-        Command, 
-        Birthday
-from packAnimalNew;
-select * from mansFriend;
+SELECT * FROM homeAnimal;
 
-create table youngAnimals (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)
-select Name, 
+INSERT INTO mansFriend (Name, Command, Birthday)
+SELECT  Name, 
+        Command, 
+        Birthday
+FROM homeAnimal UNION
+SELECT  Name, 
+        Command, 
+        Birthday
+FROM packAnimalNew;
+SELECT * FROM mansFriend;
+
+CREATE TABLE youngAnimals (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)
+SELECT Name, 
         Command, 
         Birthday,
         Round((year(current_date()) - year(Birthday)) + (month(current_date() - month(Birthday)))/10, 2) as age
-from mansFriend
-where Round((year(current_date()) - year(Birthday)) + (month(current_date() - month(Birthday)))/10, 2) > 1 
+FROM mansFriend
+WHERE Round((year(current_date()) - year(Birthday)) + (month(current_date() - month(Birthday)))/10, 2) > 1 
 	and Round((year(current_date()) - year(Birthday)) + (month(current_date() - month(Birthday)))/10, 2) < 3;
-select * from youngAnimals;
 
-## Объединить все таблицы в одну, при этом сохраняя поля, указывающие на прошлую принадлежность к старым таблицам.
+SELECT * FROM youngAnimals;
 
-create table newmansFriend (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)
-select  Name, 
+-- объединить все таблицы в одну, при этом сохраняя поля, указывающие на прошлую принадлежность к старым таблицам
+CREATE TABLE newmansFriend (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY)
+SELECT  Name, 
         Command,
         Birthday,
-        'cat' as oldTable
-from cat union 
-select  Name, 
+        'cat' AS oldTable
+FROM cat UNION
+SELECT  Name, 
         Command, 
         Birthday,
-        'dog' as oldTable
-from dog union
-select  Name, 
+        'dog' AS oldTable
+FROM dog UNION
+SELECT  Name, 
         Command, 
         Birthday,
-        'hamster' as oldTable
-from hamster union 
-select  Name, 
+        'hamster' AS oldTable
+FROM hamster UNION
+SELECT  Name, 
         Command, 
         Birthday,
-        'horse' as oldTable
-from horse union 
-select  Name, 
+        'horse' AS oldTable
+FROM horse UNION 
+SELECT  Name, 
         Command, 
         Birthday,
-        'donkey' as oldTable
-from donkey;
+        'donkey' AS oldTable
+FROM donkey;
 
-select * from newmansFriend;
+SELECT * FROM newmansFriend;
